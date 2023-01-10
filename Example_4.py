707eebff37f3915697b2d48567e5be9497da9572
Vivek Patil <patilvv17@gmail.com>
2023-01-10 17:15:57 +0530

    p

---
+++ Example_4.py
@@ -0,0 +1,6 @@
+# slicing a string
+
+mystring = "Computers"
+print(mystring)
+print(mystring[2:6])  # slice from 2nd position to 6th position
+print(mystring[4:-2]) # slice from 4th position to second last position
